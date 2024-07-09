e8a6ff82723dbbc7a0bc38b0c42184bc948f40be
yunben <vietzz771@gmail.com>
2024-07-09 16:19:26 +0700

    add fancybox library

--- main.js
+++ main.js
@@ -2,10 +2,32 @@
 const mobileMenuButton = document.getElementById("mobile-menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
 
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+const overlay = document.getElementById("overlay");
+const body = document.body;
+
 mobileMenuButton.addEventListener("click", () => {
   const isExpanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
   mobileMenuButton.setAttribute("aria-expanded", !isExpanded);
   mobileMenu.classList.toggle("hidden");
+  menuIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+  if (!isExpanded) {
+    body.classList.add("overflow-hidden");
+  } else {
+    body.classList.remove("overflow-hidden");
+  }
+  mobileMenu.classList.toggle("show");
+  overlay.classList.toggle("overlay");
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("nav-scroll");
+  } else {
+    navbar.classList.remove("nav-scroll");
+  }
+});
