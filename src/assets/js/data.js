afb1f19ff5970633c2ab79e0608ce4c4858d5a35
yunben <vietzz771@gmail.com>
2024-07-12 16:36:58 +0700

    Revert "add page partnership"
    This reverts commit 4398e4a061c7efa22e359541ccda02fc35c6db85.

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -3,7 +3,11 @@ export const projects = [
     id: 1,
     name: "Neat",
     coverImage: "./src/assets/images/neat.png",
-    images: ["./src/assets/images/neat.png", "./src/assets/images/moment.png", "./src/assets/images/mugshot.png"],
+    images: [
+      "./src/assets/images/neat.png",
+      "./src/assets/images/moment.png",
+      "./src/assets/images/mugshot.png"
+    ],
   },
   {
     id: 2,
@@ -38,7 +42,7 @@ export const projects = [
     ],
   },
   {
-    id: 5,
+    id: 4,
     name: "Polka Drawer",
     coverImage: "./src/assets/images/polkadrawer.png",
     images: [
@@ -47,110 +51,82 @@ export const projects = [
       "./src/assets/images/spell.png",
       "./src/assets/images/moment.png"
     ],
-  },
-  {
-    id: 6,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 7,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 8,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 9,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 10,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 11,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-  {
-    id: 12,
-    name: "Polka Drawer",
-    coverImage: "./src/assets/images/polkadrawer.png",
-    images: [
-      "./src/assets/images/polkadrawer.png",
-      "./src/assets/images/offthegrid.png",
-      "./src/assets/images/spell.png",
-      "./src/assets/images/moment.png"
-    ],
-  },
-];
-
-export const partners = [
-  {
-    id: 1,
-    name: "Neat",
-    coverImage: "./src/assets/images/neat.png",
-  },
-  {
-    id: 2,
-    name: "Mug Shot",
-    coverImage: "./src/assets/images/mugshot.png",
-  },
-  {
-    id: 3,
-    name: "Control Center",
-    coverImage: "./src/assets/images/controlcenter.png",
   },
   {
     id: 4,
     name: "Polka Drawer",
     coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
   },
   {
-    id: 5,
+    id: 4,
     name: "Polka Drawer",
     coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
   },
+  {
+    id: 4,
+    name: "Polka Drawer",
+    coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
+  },
+  {
+    id: 4,
+    name: "Polka Drawer",
+    coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
+  },
+  {
+    id: 4,
+    name: "Polka Drawer",
+    coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
+  },
+  {
+    id: 4,
+    name: "Polka Drawer",
+    coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
+  },
+  {
+    id: 4,
+    name: "Polka Drawer",
+    coverImage: "./src/assets/images/polkadrawer.png",
+    images: [
+      "./src/assets/images/polkadrawer.png",
+      "./src/assets/images/offthegrid.png",
+      "./src/assets/images/spell.png",
+      "./src/assets/images/moment.png"
+    ],
+  }
 ];
